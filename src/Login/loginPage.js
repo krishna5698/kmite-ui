6c7757d38c7ed2b61dd66b17c1a27649b1e64e16
krishnamoorthy T <krishnamoorthy.t@stinsoft.com>
2024-11-04 10:40:49 +0530

    create all screens

---
+++ src/Login/loginPage.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    console.log("Logging in with:", { email, password });
+  };
+
+  return (
+    <div className="login-page">
+      <div className="login-container">
+        <div className="login-box">
+          <h2>Welcome Back!</h2>
+          <p>Sign in to continue to MKM Instutions now.</p>
+          <form className="login-form" onSubmit={handleSubmit}>
+            <div className= "form-group">
+              <label htmlFor="email">Email</label>
+              <div className="input-container">
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Enter email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="form-group ">
+              <label htmlFor="password">Password</label>
+              <div className="input-container">
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  placeholder="Enter password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="form-group remember-container">
+              <div className="remember-me">
+                <input type="checkbox" id="remember" name="remember" />
+                <label htmlFor="remember">Remember me</label>
+              </div>
+              {/* <a href="#forgot-password" className="forgot-password-link" onClick={() => setModalOpen(true)}>  
+                Forgot password?
+              </a> */}
+            </div>
+            <button type="submit">Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
